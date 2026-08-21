347e3d6ece13d206bc738f3943d8e2deaf51bddc
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2026-08-21 15:41:37 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "EaseUI",
-      fileName: (format) => `easeui.${format}.js`,
-    },
-    cssCodeSplit: true,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-});
+  plugins: [react()],
+  base: '/EaseUi/', // Must match your GitHub repository name exactly
+})
